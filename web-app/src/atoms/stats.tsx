4fbd17b669914875387c004ef567522b4c421fff
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-14 19:05:26 +0100

    add random stats

--- web-app/src/atoms/stats.tsx
+++ web-app/src/atoms/stats.tsx
@@ -5,11 +5,12 @@ interface StatsProps {
     label: string;
     icon?: string;
     value: number | string | undefined;
+    className?: string;
 }
 
-const Stats: React.FC<StatsProps> = ({label, icon, value}) => {
+const Stats: React.FC<StatsProps> = ({label, icon, value, className = ''}) => {
     return (
-        <div className="flex flex-col items-center shadow w-fit h-fit p-4 rounded-md">
+        <div className={`flex flex-col items-center shadow p-4 rounded-md ${className}`}>
             <span className="text-sm text-gray-800 items-center flex flex-row gap-2">
                 {icon && <i className={icon}/>}
                 {label}
--- web-app/src/main/analysis.tsx
+++ web-app/src/main/analysis.tsx
@@ -14,7 +14,10 @@ const Analysis: React.FC<AnalysisProps> = ({className}) => {
     return (
         <div className={`w-full h-full p-8 justify-center items-center ${className}`}>
             <h1>Analysis</h1>
-            <Stats label="Total Cars" value={10} icon='fa fa-car'/>
+            <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+                <Stats label="Total Charge Expected (kwH)" value={10} icon='fas fa-bolt'/>
+                <Stats label="MAX Energy Spent (kwH)" value={30}/>
+            </div>
         </div>
     );
 }
